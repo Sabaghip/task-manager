476866f678fd4e0c65b6198f5f33ecda9a17ffe4
sabaghip <sabaghi.p@gmail.com>
2025-05-15 11:08:09 +0330

    fix: dependanciesfixed

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserRepo } from './user.repository';
 import { PrismaModule } from '../prisma/prisma.module';
+import { HashModule } from 'src/shared/utils/hash/hash.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, HashModule],
   providers: [UserService, UserRepo],
   exports: [UserService]
 })
